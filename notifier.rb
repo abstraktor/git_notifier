f38ede62359d63e4e3afcd766fdb76c69c5c22fe
Bastian Kruck <ich@bkruck.de>
2012-10-10 17:04:42 +0200

    now forking and immediately returning a 404 for obfuscation

--- notifier.rb
+++ notifier.rb
@@ -4,14 +4,17 @@ require 'net/http'
 set :port, 5050
 
 get '/fetch' do
-  # forward to all other listeners
-  begin
-    File.read('forward_uri').lines do |line|
-      Net::HTTP.get URI(line)
-    end
-  rescue Net::HTTPBadResponse
-  end
+	
+	fork do
+		# forward to all other listeners
+		begin
+		File.read('forward_uri').lines do |line|
+			Net::HTTP.get URI(line)
+		end
+		rescue Net::HTTPBadResponse
+		end
+		system "./fetch_hook"
+	end
 
-  system "./fetch_hook"
-  "ok"
+	not_found
 end
