8abfef5517247a036badebe4d6243cfd7d4857f0
Bastian Kruck <ich@bkruck.de>
2012-10-10 16:46:51 +0200

    added uri forwarding

--- notifier.rb
+++ notifier.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
+require 'net/http'
 
 set :port, 5050
 
 get '/fetch' do
+  # forward to all other listeners
+  File.read('forward_uri').lines do |line|
+    Net::HTTP.get URI(line)
+  end
+
   system "./fetch_hook"
   "ok"
 end
